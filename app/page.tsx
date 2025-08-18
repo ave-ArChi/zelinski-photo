13b4a4ef2dba840408ea0498a50c9b06d94bc1cb
ave-ArChi <4i4marev@gmail.com>
2025-08-18 19:27:00 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,2 @@
-export default function Home() {
-  return (
-    <div className="pt-6 text-center text-sm text-neutral-600">
-      Перейти к <a className="underline" href="/portfolio">портфолио</a>.
-    </div>
-  );
-}
+// app/page.tsx
+export { default } from './portfolio/page'; // просто используем ту же страницу
